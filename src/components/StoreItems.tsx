564091a457470420493d4c10a1571d41a2a8613a
BehzadJanNesar <behzadjannesar.2001@gmail.com>
2024-08-03 21:47:22 +0330

    add skeleton screens loading animation for the cards

--- src/components/StoreItems.tsx
+++ src/components/StoreItems.tsx
@@ -1,6 +1,15 @@
-import { Card, CardContent, CardMedia, Typography, Button, Box, Grid, Stack } from "@mui/material";
+import {
+   Card,
+   CardContent,
+   CardMedia,
+   Typography,
+   Button,
+   Box,
+   Grid,
+   Stack,
+   Skeleton,
+} from "@mui/material";
 import Vector from "../../Public/Images/Vector.svg";
-import CardImage from "../../Public/Images/CardImage.svg";
 import OpenIcon from "../../Public/Images/openIcon.svg";
 import ClockIcon from "../../Public/Images/clock-watch-svgrepo-com 4.svg";
 import CallIcon from "../../Public/Images/callIcon.svg";
@@ -17,39 +26,8 @@ interface ItemStoreProps {
    phone_number: string;
    repair_service: string;
    price: string;
+   loading: boolean;
 }
-
-function convertToIranianPrice(number: string): string {
-   const persianDigits = "۰۱۲۳۴۵۶۷۸۹";
-   const result = Math.trunc(Number(number) / 10);
-   const persianNumber = result.toString().replace(/\d/g, (d) => persianDigits[parseInt(d)]);
-   return persianNumber;
-}
-function convertRate(number: string): string {
-   const persianDigits = "۰۱۲۳۴۵۶۷۸۹";
-   const persianNumber = number.toString().replace(/\d/g, (d) => persianDigits[parseInt(d)]);
-   return persianNumber;
-}
-
-interface PhoneNumberDisplayProps {
-   phoneNumber: string;
-}
-
-const convertToIranianPhoneNumber = (phoneNumber: string): string => {
-   const persianDigits = "۰۱۲۳۴۵۶۷۸۹";
-   const convertDigits = (number: string): string =>
-      number.replace(/\d/g, (d) => persianDigits[parseInt(d)]);
-
-   if (phoneNumber.startsWith("+98")) {
-      return `+۹۸${convertDigits(phoneNumber.slice(3))}`;
-   } else if (phoneNumber.startsWith("09")) {
-      return `۰${convertDigits(phoneNumber.slice(1))}`;
-   } else {
-      return convertDigits(phoneNumber);
-   }
-};
-
-console.log(convertToIranianPhoneNumber("09359227339"));
 
 export default function ItemStore({
    img,
@@ -61,68 +39,134 @@ export default function ItemStore({
    rate,
    repair_service,
    price,
+   loading,
 }: ItemStoreProps) {
    return (
       <Card sx={{ mt: 3, marginInline: "auto", padding: "30px", boxShadow: "unset" }}>
-         <CardMedia
-            component="img"
-            width="360px"
-            height="180px"
-            image={img}
-            alt="Bruce's Tire & Auto Service"
-            sx={{ borderRadius: "5px" }}
-         />
+         {loading ? (
+            <Skeleton
+               sx={{ borderRadius: "5px", backgroundColor: "#f1f1f1" }}
+               animation="wave"
+               variant="rectangular"
+               width="100%"
+               height={180}
+            />
+         ) : (
+            <CardMedia
+               component="img"
+               width="360px"
+               height="180px"
+               image={img}
+               alt="Bruce's Tire & Auto Service"
+               sx={{ borderRadius: "5px" }}
+            />
+         )}
          <CardContent sx={{ padding: 0, marginTop: "10px", borderBottom: "1px solid #6C6C6C4A" }}>
             <Box display="flex" alignItems="center" justifyContent="space-between" mb={0.5}>
-               <Typography variant="h3" component="div">
-                  {title}
-               </Typography>
-               <Box component="div" display="flex" alignItems="center" gap={0.5}>
-                  <Typography component="p" variant="h3" color="#424242" className="font-[18px]">
-                     {convertRate(rate)}
+               {loading ? (
+                  <Skeleton
+                     sx={{ borderRadius: "5px", backgroundColor: "#f1f1f1" }}
+                     animation="wave"
+                     height={20}
+                     width="70%"
+                     style={{ marginBottom: "4px" }}
+                  />
+               ) : (
+                  <Typography variant="h3" component="div">
+                     {title}
                   </Typography>
-                  <Box component="img" src={Vector} sx={{ paddingBottom: "5px" }} />
-               </Box>
+               )}
+               {loading ? (
+                  <Skeleton
+                     sx={{ borderRadius: "5px", backgroundColor: "#f1f1f1" }}
+                     animation="wave"
+                     height={20}
+                     width="20%"
+                     style={{ marginBottom: "4px" }}
+                  />
+               ) : (
+                  <Box component="div" display="flex" alignItems="center" gap={0.5}>
+                     <Typography component="p" variant="h3" color="#424242" className="font-[18px]">
+                        {rate}
+                     </Typography>
+                     <Box component="img" src={Vector} sx={{ paddingBottom: "5px" }} />
+                  </Box>
+               )}
             </Box>
-            <Typography variant="body2" color="text.secondary" mb={0.5}>
-               {address}
-            </Typography>
-            <Grid container spacing={1} alignItems="center" mb={0.5}>
-               <Grid sx={{ display: "flex", alignItems: "center" }} item>
-                  <Box component="img" src={ClockIcon} />
-               </Grid>
-               <Grid item>
-                  <Typography variant="body2" color="text.secondary">
-                     {openTime}
-                  </Typography>
-               </Grid>
-            </Grid>
-            <Grid container spacing={1} alignItems="center" mb={0.5}>
-               <Grid sx={{ display: "flex", alignItems: "center" }} item>
-                  <Box component="img" src={OpenIcon} />
-               </Grid>
-               <Grid item>
-                  <Typography variant="body2" color="text.secondary">
-                     {sunestTime}
-                  </Typography>
-               </Grid>
-            </Grid>
-            <Grid container spacing={1} alignItems="center" mb={0.5}>
-               <Grid sx={{ display: "flex", alignItems: "center" }} item>
-                  <Box component="img" src={CallIcon} />
-               </Grid>
-               <Grid item>
-                  <Typography variant="body2" color="text.secondary">
-                     {phone_number
-                        ? convertToIranianPhoneNumber(phone_number)
-                        : "شماره همراهی ثبت نشده است"}
-                  </Typography>
-               </Grid>
-            </Grid>
-            <Grid container mt={3}>
-               <Typography variant="h3" color="primary" mb={0.5}>
-                  {repair_service}
+            {loading ? (
+               <Skeleton
+                  sx={{ borderRadius: "5px", backgroundColor: "#f1f1f1" }}
+                  animation="wave"
+                  height={20}
+                  width="70%"
+                  style={{ marginBottom: "4px" }}
+               />
+            ) : (
+               <Typography variant="body2" color="text.secondary" mb={0.5}>
+                  {address}
                </Typography>
+            )}
+            {loading ? (
+               <Skeleton
+                  sx={{ borderRadius: "5px", backgroundColor: "#f1f1f1" }}
+                  animation="wave"
+                  height={20}
+                  width="70%"
+                  style={{ marginBottom: "4px" }}
+               />
+            ) : (
+               <Grid container spacing={1} alignItems="center" mb={0.5}>
+                  <Grid sx={{ display: "flex", alignItems: "center" }} item>
+                     <Box component="img" src={ClockIcon} />
+                  </Grid>
+                  <Grid item>
+                     <Typography variant="body2" color="text.secondary">
+                        {openTime}
+                     </Typography>
+                  </Grid>
+               </Grid>
+            )}
+            {loading ? (
+               <Skeleton
+                  sx={{ borderRadius: "5px", backgroundColor: "#f1f1f1" }}
+                  animation="wave"
+                  height={20}
+                  width="70%"
+                  style={{ marginBottom: "4px" }}
+               />
+            ) : (
+               <Grid container spacing={1} alignItems="center" mb={0.5}>
+                  <Grid sx={{ display: "flex", alignItems: "center" }} item>
+                     <Box component="img" src={OpenIcon} />
+                  </Grid>
+                  <Grid item>
+                     <Typography variant="body2" color="text.secondary">
+                        {sunestTime}
+                     </Typography>
+                  </Grid>
+               </Grid>
+            )}
+            {loading ? (
+               <Skeleton
+                  sx={{ borderRadius: "5px", backgroundColor: "#f1f1f1" }}
+                  animation="wave"
+                  height={20}
+                  width="70%"
+                  style={{ marginBottom: "4px" }}
+               />
+            ) : (
+               <Grid container spacing={1} alignItems="center" mb={0.5}>
+                  <Grid sx={{ display: "flex", alignItems: "center" }} item>
+                     <Box component="img" src={CallIcon} />
+                  </Grid>
+                  <Grid item>
+                     <Typography variant="body2" color="text.secondary">
+                        {phone_number ? phone_number : "شماره همراهی ثبت نشده است"}
+                     </Typography>
+                  </Grid>
+               </Grid>
+            )}
+            {/* <Grid container mt={3}>
                <Grid container display="flex" alignItems="center" justifyContent="space-between">
                   <Grid display="flex" alignItems="center" gap={1}>
                      <Box component="img" src={Group} />
@@ -142,14 +186,34 @@ export default function ItemStore({
                      </Typography>
                   </Grid>
                </Grid>
-            </Grid>
+            </Grid> */}
             <Stack direction={"row-reverse"} spacing={1} sx={{ mt: 3 }}>
-               <Button sx={{ flex: 2 }} variant="outlined" color="primary" fullWidth>
-                  <Box component="img" src={MapIcon} />
-               </Button>
-               <Button sx={{ flex: 10 }} variant="contained" color="primary" fullWidth>
-                  بیشتر...
-               </Button>
+               {loading ? (
+                  <Skeleton
+                     sx={{ borderRadius: "5px", backgroundColor: "#f1f1f1" }}
+                     animation="wave"
+                     height={60}
+                     width="30%"
+                     style={{ marginBottom: "4px" }}
+                  />
+               ) : (
+                  <Button sx={{ flex: 2 }} variant="outlined" color="primary" fullWidth>
+                     <Box component="img" src={MapIcon} />
+                  </Button>
+               )}
+               {loading ? (
+                  <Skeleton
+                     sx={{ borderRadius: "5px", backgroundColor: "#f1f1f1" }}
+                     animation="wave"
+                     height={60}
+                     width="90%"
+                     style={{ marginBottom: "4px" }}
+                  />
+               ) : (
+                  <Button sx={{ flex: 10 }} variant="contained" color="primary" fullWidth>
+                     بیشتر...
+                  </Button>
+               )}
             </Stack>
          </CardContent>
       </Card>
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -3,8 +3,11 @@ import ItemStore from "../components/StoreItems";
 import { useMemo } from "react";
 
 export default function Store() {
+   const loading = false;
    const memo = useMemo(() => {
-      return StoreItems.CardDetails.map((item, index) => <ItemStore {...item} key={index} />);
+      return StoreItems.CardDetails.map((item, index) => (
+         <ItemStore loading={loading} {...item} key={index} />
+      ));
    }, [StoreItems.CardDetails.length]);
    return <div style={{ margin: "auto", width: "100%" }}>{memo}</div>;
 }
